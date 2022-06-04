e245a59cca1237d988a95a0d61bc0a01fd992c9f
R.V.Rajagopalan <rajagopalan@RVRajagopalans-MacBook-Pro.local>
2022-06-05 01:51:37 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,75 @@
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  Routes
+} from "react-router-dom";
 
 function App() {
 
   const [mode, setMode] = useState('light')
   const [alert, setAlert] = useState(null)
 
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000)
+  }
+
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enabled", "success")
     }
     else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled", "success")
     }
   }
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
-      <div className="container my-4">
-        <TextForm heading="Enter the text to analyze below" mode={mode}/>
-      </div>
+      {/* <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-4">
+          <Routes>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />
+            </Route>
+          </Routes>
+        </div>
+      </Router> */}
+      <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-4">
+          <Routes>
+          <Route exact path="/about" element={<About />}>
+          </Route>
+          <Route exact path="/"
+            element={<TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />}>
+          </Route>
+        </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 export default function Alert(props) {
     return (
-        <div>
-            <div className="alert alert-warning alert-dismissible fade show" role="alert">
-                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
+        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+            <strong>{props.alert.type}</strong>: {props.alert.message}
         </div>
     )
 }
