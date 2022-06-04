fbb95d4e31208cdccf16c45b56e14790903977d1
R.V.Rajagopalan <rajagopalan@RVRajagopalans-MacBook-Pro.local>
2022-06-04 14:06:35 +0530

    readme editted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,17 +33,6 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            {/* <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-primary" type="submit">
-                Search
-              </button>
-            </form> */}
             <div className={`form-check form-switch text-${(props.mode === 'light'?'dark':'light')}`}>
               <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
                 <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Turn {(props.mode === 'light'?'on':'off')} dark mode</label>
